9171e4024f0f2b0359154b56b40a2d6616cf84e7
Francisco Miguel Lopes Requicha <66685912+franciscolopes26@users.noreply.github.com>
2021-07-15 02:44:04 -0700

    Add files via upload

---
+++ crud_api/api/app.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'headers.php';
+$conn = new mysqli('localhost', 'root', '',  'btracker');
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET['id'])) {
+        $id = $conn->real_escape_string($_GET['id']);
+        $sql = $conn->query("SELECT * FROM carga WHERE id = '$id'");
+        $data = $sql->fetch_assoc();
+    } else {
+        $data = array();
+        $sql = $conn->query("SELECT * FROM carga");
+        while ($d = $sql->fetch_assoc()) {
+            $data[] = $d;
+        }
+    }
+    exit(json_encode($data)); // return json data
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data =  json_decode(file_get_contents("php://input"));
+    $sql = $conn->query("INSERT INTO cargas (idCargas, NumeroLoad, NumeroCMR, NomeTranspor,Matricula,NomeMotoris,Date) VALUES ('" . $data->id . "','" . $data->name . "', '" . $data->address . "', '" . $data->phone . "')");
+    if ($sql) {
+        $data->id = $conn->insert_id;
+        exit(json_encode($data));
+    } else {
+        exit(json_encode(array('status' => 'error')));
+    }
+}
+
+// if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+//     if (isset($_GET['id'])) {
+//         $id = $conn->real_escape_string($_GET['id']);
+//         $data = json_decode(file_get_contents("php://input"));
+//         $sql = $conn->query("UPDATE cargas SET name ='" . $data->name . "', address = '" . $data->address . "', phone = '" . $data->phone . "' WHERE id = '$id'");
+//         if ($sql) {
+//             exit(json_encode(array('status' => 'success')));
+//         } else {
+//             exit(json_encode(array('status' => 'error')));
+//         }
+//     }
+// }
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    if (isset($_GET['id'])) {
+        $id = $conn->real_escape_string($_GET['id']);
+        $sql = $conn->query("DELETE FROM carga WHERE id= '$id'");
+        if ($sql) {
+            exit(json_encode(array('status' => 'success')));
+        } else {
+            exit(json_encode(array('status' => 'error')));
+        }
+    }
+}
